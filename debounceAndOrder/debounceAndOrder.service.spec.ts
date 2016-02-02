787195c8ebd3258305f78667731d0d0f04d6f1b5
Nathan Friend <nathan.friend@pioneer.com>
2016-02-02 14:42:14 -0600

    added debounceAndOrder service

---
+++ debounceAndOrder/debounceAndOrder.service.spec.ts
@@ -0,0 +1,116 @@
+﻿import {DebounceAndOrderService} from './debounceAndOrder.service';
+
+export function main() {
+    describe('debounceAndOrder.service', () => {
+
+        let debounceAndOrderService: DebounceAndOrderService.DebounceAndOrderService,
+            $timeout: ng.ITimeoutService,
+            $q: ng.IQService;
+
+
+        beforeEach(() => {
+            inject(($injector: any) => {
+                $timeout = $injector.get('$timeout');
+                $q = $injector.get('$q');
+            });
+
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+            debounceAndOrderService = new DebounceAndOrderService.DebounceAndOrderService($timeout, $q);
+        });
+
+        it('should return a new function that wraps the original function', () => {
+            let originalFunction = jasmine.createSpy('originalFunction');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 0);
+            debouncedFunction();
+
+            $timeout.flush();
+            expect(originalFunction.calls.count()).toBe(1);
+        });
+
+        it('should return a new function that returns a promise that resolves after <delay> milliseconds', () => {
+            let originalFunction = jasmine.createSpy('originalFunction');
+            let thenSpy = jasmine.createSpy('thenSpy');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 10);
+            debouncedFunction().then(thenSpy);
+
+            $timeout.flush();
+            expect(originalFunction.calls.count()).toBe(1);
+            expect(thenSpy.calls.count()).toBe(1);
+        });
+
+        it('should prevent the original function from being called more than once when spammed with requests', () => {
+            let originalFunction = jasmine.createSpy('originalFunction');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 500);
+            let debouncedSpy = jasmine.createSpy('debouncedSpy', debouncedFunction).and.callThrough();
+
+            for (var i = 0; i < 50; i++) {
+                $timeout(i * 10).then(debouncedSpy);
+                $timeout.flush(i * 10 + 1);
+            }
+
+            $timeout.flush();
+
+            expect(debouncedSpy.calls.count()).toBe(50);
+            expect(originalFunction.calls.count()).toBe(1);
+        });
+
+        it('should call the original function multiple times if the calls to the debounced function are made outside of the debounced function\'s delay', () => {
+            let originalFunction = jasmine.createSpy('originalFunction');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 500);
+            let debouncedSpy = jasmine.createSpy('debouncedSpy', debouncedFunction).and.callThrough();
+
+            for (var i = 0; i < 50; i++) {
+                $timeout(i * 600).then(debouncedSpy);
+                $timeout.flush(i * 600 + 1);
+            }
+
+            $timeout.flush();
+
+            expect(debouncedSpy.calls.count()).toBe(50);
+            expect(originalFunction.calls.count()).toBe(50);
+        });
+
+        it('should only resolve the promise of the most recently made call', () => {
+            let originalFunction = jasmine.createSpy('originalFunction');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 500);
+            let allSpies: Array<jasmine.Spy> = [];
+
+            for (var i = 0; i < 50; i++) {
+                $timeout(i * 10).then(() => {
+                    let thenSpy = jasmine.createSpy('spy ' + i);
+                    debouncedFunction().then(thenSpy);
+                    allSpies.push(thenSpy);
+                });
+                $timeout.flush(i * 10 + 1);
+            }
+
+            $timeout.flush();
+
+            allSpies.forEach((spy, index) => {
+                if (index !== allSpies.length - 1) {
+                    expect(spy).not.toHaveBeenCalled();
+                } else {
+                    expect(spy.calls.count()).toBe(1);
+                }
+            });
+        });
+
+        it('should resolve the returned promise even if the original function is asynchronous (returns a promise)', () => {
+            let originalFunction = () => {
+                var deferred = $q.defer();
+                $timeout(10).then(() => { deferred.resolve() });
+                return deferred.promise;
+            }
+
+            let thenSpy = jasmine.createSpy('thenSpy');
+            let debouncedFunction = debounceAndOrderService.debounceAndOrder(originalFunction, 100);
+            debouncedFunction().then(thenSpy);
+
+            $timeout.flush(101);
+            expect(thenSpy.calls.count()).toBe(0);
+            $timeout.flush(111);
+            expect(thenSpy.calls.count()).toBe(1);
+        });
+    });
+}
---
+++ debounceAndOrder/debounceAndOrder.service.ts
@@ -0,0 +1,74 @@
+﻿export module DebounceAndOrderService {
+
+    export interface IDebounceAndOrderService {
+        debounceAndOrder<T>(func: (funcArgs: any) => ng.IPromise<T> | T, delay: number): () => ng.IPromise<T>;
+    }
+
+    // This service takes a function and creates a new "debounced" version of the function. 
+    // The debounceAndOrder<T> function is similar to Lodash's "debounce" function, except
+    // this one is ng.IPromise-friendly.
+    //
+    // The debounceAndOrder<T> function does two things:
+    //   1. Prevents the original function from being called if the 
+    //      new method has been called with the last <delay> seconds.
+    //   2. Only allows the most recent call to resolve its promise.
+    export class DebounceAndOrderService implements IDebounceAndOrderService {
+
+        static $inject = ['$timeout', '$q'];
+
+        constructor(private _$timeout: ng.ITimeoutService, private _$q: ng.IQService) {
+        }
+
+        debounceAndOrder<T>(func: (funcArgs: any) => ng.IPromise<T> | T, delay: number): () => ng.IPromise<T> {
+
+            // the promise returned from the most recent call to $timeout
+            let timeoutPromise: ng.IPromise<void> = null;
+
+            // the ID of the most recent call
+            let lastRequestId = 0;
+
+            let _this = this;
+
+            // return a *new* function that has new, "debounced" abilities.
+            return function () {
+
+                // the arguments array passed to this function
+                let args = arguments;
+
+                // if a timeout is currently running, cancel it
+                _this._$timeout.cancel(timeoutPromise);
+
+                // give this call a new request ID
+                let thisRequestId = ++lastRequestId;
+
+                // the deferred whose promise we will return 
+                // at the end of this function. We will only resolve 
+                // the deferred's promise if it was the most recent 
+                // call to the search API (we ignore old requests).
+                let deferred: ng.IDeferred<T> = _this._$q.defer();
+
+                timeoutPromise = _this._$timeout(delay);
+                timeoutPromise.then(() => {
+                    timeoutPromise = null;
+
+                    // execute the original function and wait for it to complete
+                    _this._$q.when(func.apply(_this, args))
+                        .then((result) => {
+                            if (thisRequestId === lastRequestId) {
+                                // we were the most recent call made.  resolve the promise.
+                                deferred.resolve(result);
+                            } else {
+                                // another call to this function has been made since we
+                                // initiated our call.  In this case, don't resolve the promise.
+                            }
+                        })
+                        .catch((reason: any) => {
+                            deferred.reject(reason);
+                        });
+                });
+
+                return deferred.promise;
+            };
+        };
+    }
+}
